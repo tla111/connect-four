3413d982fe039e7dc13e99b9de10b5fb93542c84
Tim La <email@example.com>
2020-03-24 16:41:06 -0400

    progress

--- main.js
+++ main.js
@@ -9,10 +9,28 @@ let gameModel = [
 ]
 
 let turn = "player1";
+let playerOne = document.querySelector("#p1");
+let playerTwo = document.querySelector("#p2");
 
 function currentPlayer(){
   if(turn === "player1"){
-    
+    playerOne = playerOne.className = "playerTurn switch1";
+  }
+  else if(turn === "player2"){
+    playerTwo = playerTwo.className = "playerTurn switch2";
   }
 }
 currentPlayer()
+
+let tower1 = document.querySelector("#col1");
+
+function insertDisk(event){
+  selectTower = event.currentTarget;
+  console.log(selectTower);
+  
+  let piece = document.createElement("div");
+  piece.className = "disk";
+  selectTower.appendChild(piece);
+}
+
+tower1.addEventListener("click", insertDisk);

cece21181886752e8a9b53c4cb0c13058483ef12
Tim La <email@example.com>
2020-03-24 12:23:59 -0400

    simplified project

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
-let player1 = document.querySelector("#playerOne");
-let player2 = document.querySelector("#playerTwo");
+let player1 = document.querySelector(".playerOne");
+let player2 = document.querySelector(".playerTwo");
 let colOne = document.querySelector("#columnOne");
+let colTwo = document.querySelector("#columnTwo");
+let colThree = document.querySelector("#columnThree");
+let colFour = document.querySelector("#columnFour");
+let colFive = document.querySelector("#columnFive");
+let colSix = document.querySelector("#columnSix");
+let colSeven = document.querySelector("#columnSeven");
+
 
 let mode = "pickUp";
-let columnMove;
+let column;
 let gamePiece;
-let counter;
 
-// function moveToColumnOne(event){
-//   if(mode === "pickUp"){
-//     columnMove = event.currentTarget;
-//     gamePiece = columnMove.firstElementChild;
-//     mode = "drop";
-//     return mode, columnMove, gamePiece;
-//   }
-//   if(mode === "drop"){
-//     let selectedColumn = event.currentTarget;
-//     if(selectedColumn.childElementCount === 6){
-//       selectedColumn = selectedColumn.lastElementChild;
-//       selectedColumn = selectedColumn.className = " square fill";
-//       mode = "pickUp";
-//     }
-//   }
-// }
+function moveToColumn(event){
+  if(mode === "pickUp"){
+    column = event.currentTarget;
+    gamePiece = column.lastElementChild;
+    console.log(column);
+    
+    mode = "drop";
+    return mode, column, gamePiece;
+  }
+  if(mode === "drop"){
+    let column = event.currentTarget;
+    column.appendChild(gamePiece);
+    mode = "pickUp";
+  }
+}
 
-player1.addEventListener("click", moveToColumnOne);
-player2.addEventListener("click", moveToColumnOne);
-colOne.addEventListener("click", moveToColumnOne);
+player1.addEventListener("click", moveToColumn);
+player2.addEventListener("click", moveToColumn);
+colOne.addEventListener("click", moveToColumn);
+colTwo.addEventListener("click", moveToColumn);
+colThree.addEventListener("click", moveToColumn);
+colFour.addEventListener("click", moveToColumn);
+colFive.addEventListener("click", moveToColumn);
+colSix.addEventListener("click", moveToColumn);
+colSeven.addEventListener("click", moveToColumn);

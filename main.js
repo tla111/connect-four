a3e49fbca6117743e35cdfb7e3b74ec8526a8518
Tim La <email@example.com>
2020-03-24 18:53:36 -0400

    toggle turn

--- main.js
+++ main.js
@@ -1,27 +1,16 @@
 let gameModel = [
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0]
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0]
 ]
 
 let turn = "player1";
 let playerOne = document.querySelector("#p1");
 let playerTwo = document.querySelector("#p2");
-
-// function currentPlayer(){
-//   if(turn === "player1"){
-//     playerOne = playerOne.className = "playerTurn switch1";
-//   }
-//   else if(turn === "player2"){
-//     playerTwo = playerTwo.className = "playerTurn switch2";
-//   }
-// }
-// currentPlayer()
-
 
 let tower1 = document.querySelector("#col1");
 let tower2 = document.querySelector("#col2");
@@ -31,19 +20,21 @@ let tower5 = document.querySelector("#col5");
 let tower6 = document.querySelector("#col6");
 let tower7 = document.querySelector("#col7");
 
-function insertDisk(event){
+function insertDisk(event) {
   let selectTower = event.currentTarget;
 
-  if((turn === "player1") && (selectTower.childElementCount < 6)){
-  
+  if ((turn === "player1") && (selectTower.childElementCount < 6)) {
+    playerTwo.className = "playerTurn switch2";
+    playerOne.className = "blank";
     selectTower = event.currentTarget;
     let piece = document.createElement("div");
     piece.className = "diskBlack";
     selectTower.appendChild(piece);
     turn = "player2";
   }
-  else if ((turn === "player2") && (selectTower.childElementCount < 6)){
-  
+  else if ((turn === "player2") && (selectTower.childElementCount < 6)) {
+    playerOne.className = "playerTurn switch1";
+    playerTwo.className = "blank";
     selectTower = event.currentTarget;
     let piece = document.createElement("div");
     piece.className = "diskRed";
@@ -59,11 +50,3 @@ tower4.addEventListener("click", insertDisk);
 tower5.addEventListener("click", insertDisk);
 tower6.addEventListener("click", insertDisk);
 tower7.addEventListener("click", insertDisk);
-
-
-
-// if(selectTower.childElementCount < 6){
-//   selectTower.appendChild(piece);
-// }else{
-//   alert("Column is Filled")
-// }
